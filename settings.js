e20cee921bc39bd642a7402eb382e5b07529fc44
Kennwalker <kennwalker222@gmail.com>
2025-09-09 16:30:33 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝐌𝐚𝐬𝐤𝐲_𝐌𝐃',
+  author: '𝐌𝐚𝐬𝐤𝐲 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐓𝐞𝐜𝐡',
+  botName: "𝗠𝗔𝗦𝗞𝗬 𝗠𝗗",
+  botOwner: 'Moe OFFICIAL TECH', // Your name
+  ownerNumber: '12136061765', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "private",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.1.6",
+  updateZipUrl: "https://github.com/MaskyOfficialTech/MASKY_MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
